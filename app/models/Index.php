3e79867cfd93acc867e8a8fb22aa1292691e56cd
Eggcode39 <cesar.ruiz39124@gmail.com>
2018-08-05 22:38:17 -0500

    Archivos Iniciales
    Estructura de carpetas basica

---
+++ app/models/Index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CesarJose39
+ * Date: 05/08/2018
+ * Time: 22:37
+ */
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CesarJose39
+ * Date: 05/08/2018
+ * Time: 20:45
+ */
+

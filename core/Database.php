1659104f696eb40bbe0f5a365188008822901cfb
Eggcode39 <cesar.ruiz39124@gmail.com>
2018-08-08 16:23:55 -0500

    Casi Nada

---
+++ core/Database.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CesarJose39
+ * Date: 06/08/2018
+ * Time: 17:49
+ */
--- index.php
+++ index.php
@@ -6,4 +6,12 @@
  * Time: 20:45
  */
 
-echo 'Hola';
+function exception_error_handler($severidad, $mensaje, $fichero, $línea) {
+    if (!(error_reporting() & $severidad)) {
+        // Este código de error no está incluido en error_reporting
+        return;
+    }
+    //throw new ErrorException($mensaje, 0, $severidad, $fichero, $línea);
+}
+
+set_error_handler("exception_error_handler");
